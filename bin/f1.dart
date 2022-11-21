1cef4956fedc7865b710ead47cff4afe942ab8d3
ak-9t6 <115596411+ak-9t6@users.noreply.github.com>
2022-11-21 13:20:55 +0530

    first commit

---
+++ bin/f1.dart
@@ -0,0 +1,14 @@
+void main(){
+  print('MY DETAILS');
+  String name = 'ananthu';
+  int age = 20;
+  double mark = 5.6;
+  String email = 'ananthakrishnang209@gmail.com';
+  int phone = 7025590900;
+  print(' name);
+  print(age);
+  print(mark);
+  print(email);
+  print(phone);
+
+}
---
+++ bin/novproject.dart
@@ -0,0 +1,16 @@
+void main(){
+  print('_______MY MOBILE DETAILS_______');
+  String model = 'Xiaomi K20';
+  int ram = 6;
+  int storage = 128;
+  String connectivity = '4G';
+  String processor = 'SD 730';
+  int sim_slots = 2;
+  print('my phone is $model');
+  print('RAM : $ram GB');
+  print('connectivity : $connectivity ');
+  print('processor : $processor ');
+  print('storage : $storage GB');
+  print('sim slots : $sim_slots');
+
+}
